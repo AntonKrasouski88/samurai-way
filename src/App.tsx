a58af691da69331bef3b9ffcacf56dd7d0b91d99
AntonKrasouski <antohkad21@gmail.com>
2022-07-21 23:16:48 +0300

    Add img

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import LogoSamuraiWay from "./img/way-of-the-samurai.jpg"
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className={"app-wrapper"}>
+            <header>
+                <img src={LogoSamuraiWay} alt={"logo_way_of_the_samurai"}/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div>Main content</div>
+        </div>
+    );
 }
 
-export default App;
+    export default App;
