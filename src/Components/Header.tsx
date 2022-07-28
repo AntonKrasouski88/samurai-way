6f0cf65bb1c78a9a71c3008d649709ee6a23c917
AntonKrasouski <antohkad21@gmail.com>
2022-07-28 07:23:48 +0300

    Add Header.module, Navbar module, Profile.module

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,8 +3,8 @@ import LogoSamuraiWay from "../img/logo_way-of-the-samurai.jpg";
 
 export const Header = () => {
     return (
-        <header className={"header"}>
-            <img className={"header_logo"} src={LogoSamuraiWay} alt={"logo_way_of_the_samurai"}/>
+        <header className= 'head.header'>
+            <img className= "head.header_logo" src={LogoSamuraiWay} alt={"logo_way_of_the_samurai"}/>
         </header>
     );
 };
