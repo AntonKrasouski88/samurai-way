6616869360a269fce5569bccab4cc961eb832ecc
AntonKrasouski <antohkad21@gmail.com>
2022-08-07 22:21:27 +0300

    feat: add components Dialog, Message

---
+++ src/Components/Dialogs/Dialogs.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Dialogs = () => {
+    return (
+        <div>
+            <div className={'dialogs'}>
+            <div className={'dialogs-item'}>
+                <div className={'dialog'}>
+                    Dimych
+                </div>
+                <div className={'dialog'}>
+                    Albert
+                </div>
+                <div className={'dialog'}>
+                    Tanya
+                </div>
+                <div className={'dialog'}>
+                    Ilan
+                </div>
+            </div>
+        </div>
+            <div className={'messages'}>
+                <div className={'message'}>What's up bro </div>
+                <div className={'message'}>How are your it-kamasutra?</div>
+                <div className={'message'}>What do yuo do?</div>
+            </div>
+        </div>
+    );
+};
